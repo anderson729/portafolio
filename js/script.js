d3ce8f897ec2bfea508729afd13f9ae5dc314d92
anderson729 <andersonblanco729@gmail.com>
2022-08-01 15:03:07 -0500

    Se agrega portafolio

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//funcion menu responsive
+function responsiveMenu(){
+    var x = document.getElementById("menu");
+    if(x.className===""){
+        x.className = "responsive";
+    }else{
+        x.className = "";
+    }
+}
